8ef3d467d91a80c81ed7c9a1bb8bcd9524f3bc07
amiiirdara <amiiirdaraatoffice@gmail.com>
2025-01-03 19:37:11 +0330

    Update URL patterns to remove trailing slashes.
    Removed trailing slashes from all URL patterns in `product/urls.py` and `user/urls.py` for consistency and alignment with API design. This change ensures uniformity across routes and may help prevent mismatched route issues.

--- product/urls.py
+++ product/urls.py
@@ -10,11 +10,11 @@ from product.views.item_view import (
 )
 
 urlpatterns = [
-    path("categories/", CategoryListView.as_view(), name="category-list"),
-    path("images/upload/", ImageUploadView.as_view(), name="image-upload"),
-    path("images/<int:image_id>/", ImageRawView.as_view(), name="image-raw"),
+    path("categories", CategoryListView.as_view(), name="category-list"),
+    path("images/upload", ImageUploadView.as_view(), name="image-upload"),
+    path("images/<int:image_id>", ImageRawView.as_view(), name="image-raw"),
     path("items/profile", ItemListView.as_view(), name="profile-item-list"),
     path("items/create", ItemCreateView.as_view(), name="create-item"),
-    path("items/<int:item_id>/", ItemDetailView.as_view(), name="item-detail"),
-    path("items/", ItemListAllView.as_view(), name="item-list-all"),
+    path("items/<int:item_id>", ItemDetailView.as_view(), name="item-detail"),
+    path("items", ItemListAllView.as_view(), name="item-list-all"),
 ]
