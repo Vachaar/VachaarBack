dc6928079eb68b292086862ee4a6af7c1964cec6
AliTaghipour1 <alitaghipour831@gmail.com>
2025-01-29 18:50:40 +0000

    changes tls config

--- SystemTest/main.go
+++ SystemTest/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"time"
 )
@@ -12,9 +14,24 @@ const (
 )
 
 func main() {
+	var (
+		conn *tls.Conn
+		err  error
+	)
 	url := createUrl("product/items/1")
 	fmt.Println("script started,", url)
-	c := http.Client{Timeout: 10 * time.Second}
+	tlsConfig := http.DefaultTransport.(*http.Transport).TLSClientConfig
+
+	c := &http.Client{
+		Timeout: 10 * time.Second,
+		Transport: &http.Transport{
+			DialTLS: func(network, addr string) (net.Conn, error) {
+				conn, err = tls.Dial(network, addr, tlsConfig)
+				return conn, err
+			},
+		},
+	}
+
 	get, err := c.Get(url)
 	if err != nil {
 		panic(fmt.Errorf("DoClient failed: %v", err))
