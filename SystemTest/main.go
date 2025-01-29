a5947b50b1fac40fb2421ee655001e0d1ca718a8
AliTaghipour1 <alitaghipour831@gmail.com>
2025-01-29 18:26:52 +0000

    Adds sending request to server

--- SystemTest/main.go
+++ SystemTest/main.go
@@ -10,21 +10,21 @@ const (
 
 func main() {
 	fmt.Println("script started")
-	//request, err := http.NewRequest("GET", createUrl("product/items/1"), nil)
-	//if err != nil {
-	//	panic(fmt.Errorf("NewRequest failed: %v", err))
-	//}
-	//do, err := http.DefaultClient.Do(request)
-	//if err != nil {
-	//	panic(fmt.Errorf("DoClient failed: %v", err))
-	//}
-	//
-	//all, err := io.ReadAll(do.Body)
-	//if err != nil {
-	//	panic(fmt.Errorf("ReadAll all failed: %v", err))
-	//}
-	//fmt.Println(do.StatusCode)
-	//fmt.Println(string(all))
+	request, err := http.NewRequest("GET", createUrl("product/items/1"), nil)
+	if err != nil {
+		panic(fmt.Errorf("NewRequest failed: %v", err))
+	}
+	do, err := http.DefaultClient.Do(request)
+	if err != nil {
+		panic(fmt.Errorf("DoClient failed: %v", err))
+	}
+	
+	all, err := io.ReadAll(do.Body)
+	if err != nil {
+		panic(fmt.Errorf("ReadAll all failed: %v", err))
+	}
+	fmt.Printlf("status code was [%v]\n",do.StatusCode)
+	fmt.Printlf("response is [%v]\n",string(all))
 }
 
 func createUrl(route string) string {
