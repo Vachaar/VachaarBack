3ca61f3a352e011a729c77ab5f6dbea0027d3f34
AliTaghipour1 <alitaghipour831@gmail.com>
2025-01-29 18:09:33 +0000

    Adds go script for system test

---
+++ SystemTest/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	BaseUrl = "http://localhost:8080"
+)
+
+func main() {
+	fmt.Println("script started")
+	//request, err := http.NewRequest("GET", createUrl("product/items/1"), nil)
+	//if err != nil {
+	//	panic(fmt.Errorf("NewRequest failed: %v", err))
+	//}
+	//do, err := http.DefaultClient.Do(request)
+	//if err != nil {
+	//	panic(fmt.Errorf("DoClient failed: %v", err))
+	//}
+	//
+	//all, err := io.ReadAll(do.Body)
+	//if err != nil {
+	//	panic(fmt.Errorf("ReadAll all failed: %v", err))
+	//}
+	//fmt.Println(do.StatusCode)
+	//fmt.Println(string(all))
+}
+
+func createUrl(route string) string {
+	return fmt.Sprintf("%s/%s", BaseUrl, route)
+}
